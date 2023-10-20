a505295d032561b5409269bc38c7ccea18450fce
carldamey <carldamey@gmail.com>
2023-10-20 15:53:02 -0700

    updates schema with defaults and validation

--- models/flight.js
+++ models/flight.js
@@ -3,10 +3,28 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const flightSchema = new Schema({
-    airline: String,
-    airport: String,
-    flightNo: Number,
-    departs: Date,
+    airline: {
+        type: String,
+        enum: ["American", "Southwest", "Delta", "United", "Alaskan"],
+    },
+    airport: {
+        type: String,
+        enum: ["AUS", "DFW", "DEN", "LAX", "SAN", "SEA"],
+        default: "SEA",
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999,
+    },
+    departs: {
+        type: Date,
+        default: function() {
+            const currentDate = new Date()
+            currentDate.setFullYear(currentDate.getFullYear() + 1)
+            return currentDate
+        }
+    },
 }, {
     timestamps: true
 })
